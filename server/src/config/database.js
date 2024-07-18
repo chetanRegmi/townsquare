7264110e67a4f50c2d61c7907b170d72f9c9d881
chetanRegmi <chetanregmi5@gmail.com>
2024-07-18 15:33:58 +0800

    lets see if this will connect to database

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -3,14 +3,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.POSTGRES_URL,
-    process.env.POSTGRES_USER,
-    process.env.POSTGRES_PASSWORD,
-    {
-        host: process.env.POSTGRES_HOST,
-        dialect: process.env.POSTGRES_DIALECT,
-    }
-);
+// const sequelize = new Sequelize(
+//     process.env.POSTGRES_URL,
+//     process.env.POSTGRES_USER,
+//     process.env.POSTGRES_PASSWORD,
+//     {
+//         host: process.env.POSTGRES_HOST,
+//         dialect: process.env.POSTGRES_DIALECT,
+//         dialectOptions: {
+//             require: true,
+//             rejectUnauthorized: false,
+//         },
+//     }
+// );
+const sequelize = new Sequelize('postgres://default:3kAQZiWeED6R@ep-curly-credit-a4f8om20.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require');
 
 export default sequelize;
