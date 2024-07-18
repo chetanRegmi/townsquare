349514995667dd1e5e7b107491923a4f3e2d8d95
chetanRegmi <chetanregmi5@gmail.com>
2024-07-18 15:55:33 +0800

    it works now when connecting to postgres stored on vercel itself

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -4,19 +4,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const sequelize = new Sequelize(
-//     process.env.POSTGRES_URL,
-//     process.env.POSTGRES_USER,
-//     process.env.POSTGRES_PASSWORD,
-//     {
-//         host: process.env.POSTGRES_HOST,
-//         dialect: process.env.POSTGRES_DIALECT,
-//         dialectOptions: {
-//             require: true,
-//             rejectUnauthorized: false,
-//         },
-//     }
-// );
 const sequelize = new Sequelize('postgres://default:3kAQZiWeED6R@ep-curly-credit-a4f8om20.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require', {
     dialectModule: pg
 });
